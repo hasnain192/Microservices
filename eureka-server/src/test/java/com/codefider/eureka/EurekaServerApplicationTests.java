04a4a70a1d6edfd2f417037d39f1b25b0285c3a1
hasnain192 <hasnainraza192@gmail.com>
2022-05-07 15:08:57 +0530

    Change in application.yml eureka-servies

--- eureka-server/src/test/java/com/codefider/eureka/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/codefider/eureka/EurekaServerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.codefider.eureka;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
